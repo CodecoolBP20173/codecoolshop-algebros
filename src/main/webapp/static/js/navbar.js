f84a2f6483750259e9b7d53421b304d4b4a0e53f
Erik Szigeti <erik.szigeti01@gmail.com>
2018-04-24 14:49:31 +0200

    removed duplicated code

--- src/main/webapp/static/js/navbar.js
+++ src/main/webapp/static/js/navbar.js
@@ -4,8 +4,11 @@ function CategoriesDropdown() {
     document.getElementById("CategoriesDropdown").classList.toggle("show");
 }
 
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
+function SuppliersDropdown() {
+    document.getElementById("SuppliersDropdown").classList.toggle("show");
+}
+
+window.onclick = function (event) {
     if (!event.target.matches('.dropbtn')) {
 
         var dropdowns = document.getElementsByClassName("dropdown-content");
@@ -18,21 +21,3 @@ window.onclick = function(event) {
         }
     }
 };
-function SuppliersDropdown() {
-    document.getElementById("SuppliersDropdown").classList.toggle("show");
-}
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
-}
