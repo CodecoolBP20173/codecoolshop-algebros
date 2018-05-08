098ef31c2bd194933a52aa2b92761ebe02b9cb6f
Erik Szigeti <erik.szigeti01@gmail.com>
2018-05-08 14:35:50 +0200

    Renamed class

--- src/test/java/com/codecool/shop/dao/implementation/ProductCategoryDaoTest.java
+++ src/test/java/com/codecool/shop/dao/implementation/ProductCategoryDaoTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class CategoryDaoMemTest {
+class ProductCategoryDaoTest {
     private static ProductCategoryDao productCategoryDao;
 
     @BeforeEach
