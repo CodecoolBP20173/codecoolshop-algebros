8660e34c65cdb24fb78991251fb7c1d05edfdb8e
Erik Szigeti <erik.szigeti01@gmail.com>
2018-05-08 10:56:04 +0200

    Changed class for package-private

--- src/test/java/com/codecool/shop/dao/implementation/CategoryDaoMemTest.java
+++ src/test/java/com/codecool/shop/dao/implementation/CategoryDaoMemTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class CategoryDaoMemTest {
+class CategoryDaoMemTest {
     private static ProductCategoryDaoMem productCategoryDaoMem = ProductCategoryDaoMem.getInstance();
 
     @BeforeAll
