963f721b737a68fd57fac68632604bc3bea79fb9
Gergely Kovacs <gergelyk89@gmail.com>
2018-05-08 10:48:42 +0200

    registration page set up with css and js

--- src/main/java/com/codecool/shop/controller/Registration.java
+++ src/main/java/com/codecool/shop/controller/Registration.java
@@ -2,8 +2,6 @@ package com.codecool.shop.controller;
 
 
 import com.codecool.shop.config.TemplateEngineUtil;
-import com.codecool.shop.model.CheckoutProcess;
-import com.codecool.shop.model.Order;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 import javax.servlet.ServletException;
@@ -15,8 +13,8 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.HashMap;
 
-@WebServlet(urlPatterns = {"/checkout"})
-public class Checkout extends HttpServlet {
+@WebServlet(urlPatterns = {"/registration"})
+public class Registration extends HttpServlet {
     private static HashMap<String, String > userInfo;
 
 
@@ -24,18 +22,7 @@ public class Checkout extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
-        HttpSession session;
-        if (req.getSession(false) == null  ){
-            session = req.getSession(true);
-            session.setAttribute("Order", new Order());
-        } else {
-            session = req.getSession();
-        }
-        Order order = (Order) session.getAttribute("Order");
-        context.setVariable("shoppingCart", order.getCartItems());
-        double sumPrice = order.totalPrice();
-        context.setVariable("sumPrice", sumPrice);
-        engine.process("product/checkout.html", context, resp.getWriter());
+        engine.process("user/registration.html", context, resp.getWriter());
     }
 
 
@@ -47,15 +34,7 @@ public class Checkout extends HttpServlet {
         userInfo = new HashMap<>();
         userInfo.put("name",req.getParameter("name"));
         userInfo.put("email",req.getParameter("email"));
-        userInfo.put("zip",req.getParameter("zip"));
-        userInfo.put("city",req.getParameter("city"));
-        userInfo.put("country",req.getParameter("country"));
-        userInfo.put("address",req.getParameter("address"));
-        CheckoutProcess checkoutProcess = new CheckoutProcess(userInfo);
         HttpSession session = req.getSession();
-        Order order = (Order) session.getAttribute("Order");
-        checkoutProcess.process(order);
-        order.checkout(checkoutProcess);
         resp.sendRedirect("/payment");
     }
 }
