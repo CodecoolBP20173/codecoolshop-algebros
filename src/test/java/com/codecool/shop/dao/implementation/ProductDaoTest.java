fdf84cac845fc14bfc118259cbcedb95ab3216ef
Erik Szigeti <erik.szigeti01@gmail.com>
2018-05-09 13:08:36 +0200

    Tests for ProductDao

---
+++ src/test/java/com/codecool/shop/dao/implementation/ProductDaoTest.java
@@ -0,0 +1,122 @@
+package com.codecool.shop.dao.implementation;
+
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.model.Product;
+import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.model.Supplier;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductDaoTest {
+    private static ProductDao productDao;
+
+    @BeforeEach
+    void setup() {
+        productDao = ProductDaoMem.getInstance();
+        Supplier amazon = new Supplier("Amazon", "Digital content and services");
+        Supplier lenovo = new Supplier("Lenovo", "Computers and Phones");
+        Supplier samsung = new Supplier("Samsung", "Tablets, Phones and Computers");
+        ProductCategory tablet = new ProductCategory("Tablet", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
+        ProductCategory smartPhone = new ProductCategory("Smart Phone", "Hardware", "A smart phone is a small mobile computer, with the additional feature of making phone calls.");
+        ProductCategory notebook = new ProductCategory("Notebook", "Hardware", "Portable comupters");
+
+        Product amazonFire = new Product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon);
+        Product lenovoIdeaPad = new Product("Lenovo IdeaPad Miix 700", 479, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo);
+        Product amazonFireHd8 = new Product("Amazon Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon);
+        Product lenovoX012 = new Product("Lenovo XO12", 500, "USD", "Best phone ever.", smartPhone, lenovo);
+        Product SamsungS9 = new Product("Samsung S9+", 879, "USD", "Latest Sasmung smartphone with Octa Core processor.", smartPhone, samsung);
+        Product SamsungNotebook9 = new Product("Samsung Notebook 9 15\"", 1500, "USD", "Lightweight notebook with the capabilities of a desktop PC ", notebook, samsung);
+
+        productDao.add(amazonFire);
+        productDao.add(lenovoIdeaPad);
+        productDao.add(amazonFireHd8);
+        productDao.add(lenovoX012);
+        productDao.add(SamsungS9);
+        productDao.add(SamsungNotebook9);
+
+    }
+
+    @Test
+    void gettingInstanceTest() {
+        assertEquals(ProductDaoMem.class, ProductDaoMem.getInstance().getClass());
+    }
+
+    @Test
+    void checkIfGetAllDoesNotReturnReferenceTest() {
+        List products = productDao.getAll();
+        try {
+            products.remove(1);
+        } catch (UnsupportedOperationException e) {
+            assertEquals(UnsupportedOperationException.class, e.getClass());
+        }
+    }
+
+    @Test
+    void gettingAllProductsTest() {
+        int expectedListSize = productDao.getAll().size();
+        Supplier amazon = new Supplier("Amazon", "Digital content and services");
+        ProductCategory notebook = new ProductCategory("Notebook", "Hardware", "Portable comupters");
+        Product testProduct = new Product("Amazon Notebook Test", 12f, "USD", "none", notebook, amazon);
+        productDao.add(testProduct);
+        assertNotEquals(expectedListSize, productDao.getAll().size());
+    }
+
+    @Test
+    void addValidProductTest() {
+        Supplier amazon = new Supplier("Amazon", "Digital content and services");
+        ProductCategory notebook = new ProductCategory("Notebook", "Hardware", "Portable comupters");
+        Product testProduct = new Product("Amazon Notebook Test Number Two", 12f, "USD", "none", notebook, amazon);
+        productDao.add(testProduct);
+        List products = productDao.getAll();
+        assertTrue(products.contains(testProduct));
+    }
+
+    @Test
+    void addInvalidProductTest() {
+        assertThrows(NullPointerException.class, () -> productDao.add(null));
+    }
+
+    @Test
+    void findByValidIdTest() {
+        assertEquals(Product.class, productDao.find(1).getClass());
+    }
+
+    @Test
+    void findByInvalidIdTest() {
+        assertThrows(NullPointerException.class, () -> productDao.add(null));
+    }
+
+    @Test
+    void removeValidProductTest() {
+        int expectedLength = productDao.getAll().size();
+        productDao.remove(1);
+        assertNotEquals(expectedLength, productDao.getAll().size());
+    }
+
+    @Test
+    void removeInvalidProductTest() {
+        int expectedLength = productDao.getAll().size();
+        productDao.remove(-100);
+        assertEquals(expectedLength, productDao.getAll().size());
+    }
+
+    @Test
+    void getProductsByValidSupplier() {
+        Supplier amazon = new Supplier("Amazon", "Digital content and services");
+        List products = productDao.getBy(amazon);
+        Product product = (Product) products.get(1);
+        assertEquals(amazon, product.getSupplier());
+    }
+
+    @Test
+    void getProductByValidCategory() {
+        ProductCategory tablet = new ProductCategory("Tablet", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
+        List products = productDao.getBy(tablet);
+        Product product = (Product) products.get(1);
+        assertEquals(tablet, product.getProductCategory());
+    }
+}
