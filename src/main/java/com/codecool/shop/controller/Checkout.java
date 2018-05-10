683525d382b6c29d7bf15a4ce2e3f06d936223a6
Gergely Kovacs <gergelyk89@gmail.com>
2018-05-10 16:02:22 +0200

    checkout now uses userinfo from DB

--- src/main/java/com/codecool/shop/controller/Checkout.java
+++ src/main/java/com/codecool/shop/controller/Checkout.java
@@ -3,6 +3,8 @@ package com.codecool.shop.controller;
 import com.codecool.shop.config.TemplateEngineUtil;
 import com.codecool.shop.processes.CheckoutProcess;
 import com.codecool.shop.model.Order;
+import com.codecool.shop.util.NetworkUtils;
+import com.codecool.shop.util.SqlUserUtils;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -21,32 +23,32 @@ public class Checkout extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
-        HttpSession session;
-        if (req.getSession(false) == null) {
-            session = req.getSession(true);
-            session.setAttribute("Order", new Order());
-        } else {
-            session = req.getSession();
-        }
+        HttpSession session = NetworkUtils.getHTTPSession(req);
+        String email = (String) session.getAttribute("email");
         Order order = (Order) session.getAttribute("Order");
-        context.setVariable("shoppingCart", order.getCartItems());
-        context.setVariable("sumPrice", order.getTotalPriceOfOrder());
+        HashMap<String, String> userInfo = SqlUserUtils.getUser(email);
+        context.setVariable("userinfo", userInfo);
+        //context.setVariable("shoppingCart", order.getCartItems());
+        //context.setVariable("sumPrice", order.getTotalPriceOfOrder());
         engine.process("product/checkout.html", context, resp.getWriter());
     }
 
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        HashMap<String, String> userInfo;
-        userInfo = new HashMap<>();
+        HttpSession session = NetworkUtils.getHTTPSession(req);
+        String userid = (String) session.getAttribute("userid");
+        HashMap<String, String> userInfo = new HashMap<>();
         userInfo.put("name", req.getParameter("name"));
         userInfo.put("email", req.getParameter("email"));
+        userInfo.put("phone", req.getParameter("phone"));
         userInfo.put("zip", req.getParameter("zip"));
         userInfo.put("city", req.getParameter("city"));
         userInfo.put("country", req.getParameter("country"));
         userInfo.put("address", req.getParameter("address"));
+        userInfo.put("userid", userid);
+        SqlUserUtils.updateUser(userInfo);
         CheckoutProcess checkoutProcess = new CheckoutProcess(userInfo);
-        HttpSession session = req.getSession();
         Order order = (Order) session.getAttribute("Order");
         checkoutProcess.process(order);
         resp.sendRedirect("/payment");
