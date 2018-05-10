5978431749cae54b50cec6c4d1c3b3bfde5416dc
Erik Szigeti <erik.szigeti01@gmail.com>
2018-05-10 14:32:23 +0200

    Get shopping cart items

--- src/main/java/com/codecool/shop/controller/ProductControllerJdbc.java
+++ src/main/java/com/codecool/shop/controller/ProductControllerJdbc.java
@@ -1,7 +1,6 @@
 package com.codecool.shop.controller;
 
 import com.codecool.shop.dao.Jdbc.Utils;
-import com.codecool.shop.model.Order;
 import com.codecool.shop.model.Product;
 
 import java.sql.Connection;
@@ -12,29 +11,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ProductControllerJdbc {
-    public static void add(int id,int userid,int quantity) {
-        Connection dbConnection=null;
+    public static void add(int id, int userid, int quantity) {
+        Connection dbConnection = null;
         try {
             dbConnection = Utils.getConnection();
             PreparedStatement preparedStatement = dbConnection.prepareStatement("INSERT INTO shoppingcart (userid,productid,quantity) VALUES (?,?,?)");
             preparedStatement.setInt(1, userid);
             preparedStatement.setInt(2, id);
             preparedStatement.setInt(3, quantity);
-            preparedStatement.executeUpdate(); } catch (SQLException e) {
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
-        public static void update(int quantity,int id) {
-            Connection dbConnection=null;
-            try {
-                dbConnection = Utils.getConnection();
-                PreparedStatement preparedStatement = dbConnection.prepareStatement("UPDATE shoppingcart SET quantity=? WHERE productid=?");
-                preparedStatement.setInt(1, quantity);
-                preparedStatement.setInt(2, id);
-                preparedStatement.executeUpdate(); } catch (SQLException e) {
-                e.printStackTrace();
-            }
+
+    public static void update(int quantity, int id) {
+        Connection dbConnection = null;
+        try {
+            dbConnection = Utils.getConnection();
+            PreparedStatement preparedStatement = dbConnection.prepareStatement("UPDATE shoppingcart SET quantity=? WHERE productid=?");
+            preparedStatement.setInt(1, quantity);
+            preparedStatement.setInt(2, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
+    }
+
     public static int findQuantity(int id) {
 
         try {
@@ -49,47 +52,31 @@ public class ProductControllerJdbc {
                 return 0;
             }
 
-        } catch(SQLException e){
-            e.printStackTrace();
-        }
-
-        return 0;
-    }
-    /*public static Product findProductByProductId(int id){
-        try {
-            Connection dbConnection = Utils.getConnection();
-            PreparedStatement preparedStatement = dbConnection.prepareStatement("SELECT * FROM products;");
-            ResultSet resultSet = preparedStatement.executeQuery();
-            if (resultSet.next()) {
-                return ;
-            } else {
-                return 0;
-            }
-
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
         return 0;
-
     }
-    public static Product getShoppingCart() {
-        List<Product> shoppingCart=new ArrayList<>();
+
+    public static Product findProductByProductId(int id) {
+        return null;
+    }
+
+    public static List<Product> getShoppingCart() {
+        List<Product> shoppingCart = new ArrayList<>();
         try {
             Connection dbConnection = Utils.getConnection();
             PreparedStatement preparedStatement = dbConnection.prepareStatement("SELECT * FROM shoppingcart;");
             ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
-                shoppingCart.add()
-                return shoppingCart;
-            } else {
-                return 0;
+                Product product = findProductByProductId(resultSet.getInt("productid"));
+                shoppingCart.add(product);
             }
-
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
-        return 0;
-    }*/
+        return shoppingCart;
+    }
 }
