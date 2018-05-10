5b5b9e04be7403492f4269782b1229aacadc229d
Gergely Kovacs <gergelyk89@gmail.com>
2018-05-10 17:38:50 +0200

    checkout page shows cart products with totalprice

--- src/main/java/com/codecool/shop/controller/Checkout.java
+++ src/main/java/com/codecool/shop/controller/Checkout.java
@@ -27,9 +27,10 @@ public class Checkout extends HttpServlet {
         String email = (String) session.getAttribute("email");
         Order order = (Order) session.getAttribute("Order");
         HashMap<String, String> userInfo = SqlUserUtils.getUser(email);
+        String userId = (String) session.getAttribute("userid");
         context.setVariable("userinfo", userInfo);
-        //context.setVariable("shoppingCart", order.getCartItems());
-        //context.setVariable("sumPrice", order.getTotalPriceOfOrder());
+        context.setVariable("shoppingCart", OrderJdbc.getShoppingCart(Integer.parseInt(userId)));
+        context.setVariable("sumPrice", OrderJdbc.getTotalPriceOfOrder(Integer.parseInt(userId)));
         engine.process("product/checkout.html", context, resp.getWriter());
     }
 
