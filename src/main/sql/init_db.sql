21568c6d1628e4f3b0377a035412d4e7e7b2799f
Horváth Máté <ifjmate@gmail.com>
2018-05-10 17:39:36 +0200

    Modified sql to have float prices instead of integer

--- src/main/sql/init_db.sql
+++ src/main/sql/init_db.sql
@@ -29,7 +29,7 @@ CREATE TABLE products
 id INT PRIMARY KEY,
 name VARCHAR(500),
 description VARCHAR(500),
-defaultPrice INTEGER,
+defaultPrice FLOAT,
 defaultCurrency VARCHAR(500),
 productCategory VARCHAR (500),
 supplier VARCHAR (500)
@@ -64,7 +64,7 @@ CREATE TABLE orderlist
     PRIMARY KEY,
   date       DATE,
   status     VARCHAR(40),
-  totalprice INTEGER,
+  totalprice FLOAT,
   products   VARCHAR(80),
   userid     INTEGER NOT NULL
 );
